d39706ed2f09de3ad348583c0d303bce87994623
David Huang <davidhpw568@gmail.com>
2022-01-05 22:52:10 +0800

    add matlab

--- test.py
+++ test.py
@@ -3,18 +3,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D #for plotting the 3-D plot.
 import pandas as pd
+from time import sleep
+from tqdm import tqdm, trange
+import scipy.io as sio
 
-my_file = open("data13.txt", "r")
+my_file = open("/Users/davidhuang/Python/HardwareSecurity/3D random walk/data13.txt", "r")
 content = my_file.read()
 data = list(content)
-#data = [int(i) for i in data]
+ 
+I = [int(i) for i in data]
+R = np.array(I)
+
+sio.savemat('myfile.mat',{'array': R})
+
+print(R)
+print(type(R))
+
+
+"""
 matrix = 0
 m = 0
+times = 0
+progress = tqdm(total=1000)
 
 for i in range(0,255):
     for p in range(0,255):
         matrix[i,p] = data[256*i + p]
 
+progress.update(1)
+times += 1
 
 print(matrix)
 print(type(matrix))
+
+"""
